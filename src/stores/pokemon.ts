219d738b63395472ecee47415a3c2fe195bf8d63
Gabrielapu <gabrielperalta95@gmail.com>
2024-12-19 13:28:50 -0300

    detalles finales

--- src/stores/pokemon.ts
+++ src/stores/pokemon.ts
@@ -16,28 +16,29 @@ export const usePokemonStore = defineStore('pokemon', () => {
 
   const lengthSelectedPokemons = computed(() => selectedPokemons.value.length)
 
-  async function getPokemonData(id: number) {
+  async function getPokemonData(id: number): Promise<void> {
     const pokemon = await api.get(`/pokemon/${id}`)
     const species = await api.get(`/pokemon-species/${id}`)
     const respEvolutionChain = await api.get(`${species.data.evolution_chain.url.split('v2')[1]}`)
     evolutionChain.value = respEvolutionChain.data
-    detailedPokemonData.value = {...pokemon.data, flavor_text_entries: species.data.flavor_text_entries[0] }
+    detailedPokemonData.value = {
+      ...pokemon.data, 
+      flavor_text_entries: species.data.flavor_text_entries[0] 
+    }
   }
 
-  async function getPokemonList(offset: number, limit: number) {
+  async function getPokemonList(offset: number, limit: number): Promise<void> {
     const response = await api.get('/pokemon', {
       params: { offset, limit },
     })
-
     const mappedData = response.data.results.map((pokemon : Pokemon) => {
       const id = useSplitUrl(pokemon.url || '', '/');
       return { name: pokemon.name, id: parseInt(id) };
     });
-
     pokemonList.value = [...pokemonList.value, ...mappedData];
   }
 
-  async function getPokemonTeamData() {
+  async function getPokemonTeamData(): Promise<void> {
     const pokemonRequests = selectedPokemons.value.map((id: number) =>
       api.get(`/pokemon/${id}`)
     );
@@ -45,26 +46,26 @@ export const usePokemonStore = defineStore('pokemon', () => {
     pokemonTeamData.value = responses.map((response) => response.data);
   }
 
-  function resetPokemonList() {
+  function resetPokemonList(): void {
     pokemonList.value = []
   }
 
-  function deleteFromTeam(id: number) {
+  function deleteFromTeam(id: number): void {
     selectedPokemons.value = selectedPokemons.value.filter((pokemonId: number) => pokemonId !== id)
     pokemonTeamData.value = pokemonTeamData.value.filter((pokemon: Pokemon) => pokemon.id !== id)
   }
 
-  function addPokemonToTeam(pokemonId: number) {
+  function addPokemonToTeam(pokemonId: number):void {
     selectedPokemons.value.push(pokemonId)
     setLocalStorage()
   }
 
-  function removePokemonFromTeam(pokemonId: number) {
+  function removePokemonFromTeam(pokemonId: number): void {
     selectedPokemons.value = selectedPokemons.value.filter((id: any) => id !== pokemonId)
     setLocalStorage()
   }
 
-  function setLocalStorage() {
+  function setLocalStorage(): void {
     localStorage.setItem('pokemonTeam', JSON.stringify(selectedPokemons.value))
   }
 
